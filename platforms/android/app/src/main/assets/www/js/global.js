4d4d2ca5eedadc77e76f3c43ab2114b62715c299
xiao808 <zengxiao_123@163.com>
2018-03-27 20:07:10 +0800

    更改密码

---
+++ platforms/android/app/src/main/assets/www/js/global.js
@@ -0,0 +1,19 @@
+$(function () {
+	initpage();
+
+
+});
+
+
+
+/**
+ * rem布局 
+ * 
+ */
+
+function initpage()  
+  {  
+    var view_width = document.getElementsByTagName('html')[0].getBoundingClientRect().width;  
+    var _html = document.getElementsByTagName('html')[0];  
+    _html.style.fontSize =view_width/16+'px';
+  }  
