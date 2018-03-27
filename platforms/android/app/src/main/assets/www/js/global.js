c29b35de4927a8bc33a2da5641a03cbe82856af6
tanluole <tanluole@gmail.com>
2018-03-28 00:34:29 +0800

    最新页面提交

--- platforms/android/app/src/main/assets/www/js/global.js
+++ platforms/android/app/src/main/assets/www/js/global.js
@@ -1,10 +1,12 @@
 $(function () {
-	initpage();
-
-
+  //rem布局
+  initpage();
+  //平滑滚动条
+  //var IScroll = $.AMUI.iScroll;
+  //var myScroll = new IScroll('#wrapper',{
+  //  click:true
+  //});
 });
-
-
 
 /**
  * rem布局 
@@ -16,4 +18,4 @@ function initpage()
     var view_width = document.getElementsByTagName('html')[0].getBoundingClientRect().width;  
     var _html = document.getElementsByTagName('html')[0];  
     _html.style.fontSize =view_width/16+'px';
-  }  
+  }
