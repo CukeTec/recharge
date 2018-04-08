8004fbea4e7b4ef65b53db8a79a0eeb63c2df636
liudongapp <liulutte@sina.com>
2018-04-08 10:55:44 +0800

    退出功能,修改文件3个文件: 1.ActionCommand.java; 2.app.html 3.app.js 增加1个文件： LogOutExecutor.java

---
+++ platforms/android/app/src/main/assets/www/js/app.js
@@ -0,0 +1,63 @@
+
+/**
+ * document.ready不能立马执行cordova.exec   test
+ */
+document.addEventListener('deviceready', function () {
+     myapp();
+
+}, false);
+
+function myapp(){
+   cordova.exec(success, fail, "httpRequest", "MYAPPACTION", []);
+}
+
+//获取卡信息成功
+function success(msg){
+   var cardId = msg.cardId;
+   var accountState = msg.accountState;
+   var accountMoney = msg.accountMoney;
+   var userTypeInnerId = msg.userTypeInnerId;
+
+   $("#cardid").text("NO."+cardId);
+   $("#amount").text(accountMoney);
+   //0，正常，1，冻结 2，无账户信息 3 已过期
+   if(accountState === 0){
+        $("#cardstatus").text("正常");
+   }else if(accountState === 1){
+        $("#cardstatus").text("冻结");
+   }else if(accountState === 2){
+        $("#cardstatus").text("无账户信息");
+   }else if(accountState === 3){
+        $("#cardstatus").text(" 已过期");
+   }else{
+        $("#cardstatus").text(accountState);
+   }
+
+   if(userTypeInnerId === 2){
+        $("#shsq").css("display","block");
+         $("#fbxx").css("display","block");
+   }
+
+}
+
+function fail(msg){
+	alert(fail);
+}
+
+
+//退出
+$(document).ready(function(){
+
+    $("#logoutBtn").click(function(){
+         cordova.exec(succeed, failed, "httpRequest", "LOGOUT", []);
+    });
+});
+
+//登录成功
+function succeed(msg){
+	window.location.href="home.html";
+}
+
+function failed(msg){
+	alert(fail);
+}
