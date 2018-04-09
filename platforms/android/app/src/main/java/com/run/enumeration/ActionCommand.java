433be53c72a795b6fc6f63167b2fee1218044000
liudongapp <liulutte@sina.com>
2018-04-09 10:56:14 +0800

    申请接口

--- platforms/android/app/src/main/java/com/run/enumeration/ActionCommand.java
+++ platforms/android/app/src/main/java/com/run/enumeration/ActionCommand.java
@@ -28,7 +28,8 @@ public enum ActionCommand {
     SENDMESSAGE("http://sireyun.com:8081/PSMGABService/messageSend", "SendMessageExecutor"),// 发送消息接口
     LOGOUT("http://sireyun.com:8081/PSMGABService/exitLogon", "LogOutExecutor"),  //退出登录
     MESSAGEGROUP("http://sireyun.com:8081/PSMGABService/messageGroup", "MessageGroupExecutor"), // 获取消息组接口
-    ;
+    
+	;
     ActionCommand(String url, String className) {
         this.url = url;
         this.className = className;
